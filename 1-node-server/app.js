74afdad067d9219ad2d255a8a1c82a519c11534d
EriRan <erik.rantanen@hotmail.fi>
2023-01-16 13:28:14 +0200

    Start node basics module

---
+++ 1-node-server/app.js
@@ -0,0 +1,24 @@
+/**
+ * Most Node functionalities are not available by default
+ * Node ships with a few core modules
+ * http, https, fs, path, os
+ * http: launch a server, send requests
+ * https: Launch ssl encoded server
+ */
+
+// Node.js uses require to import most often?
+// I like import more
+const http = require("http");
+
+// Crucial method to creating a server
+// requestListener as an argument
+// Anonymous function provided, modern Javascript style with arrow function
+// Node.js uses event driven functionality heavily
+const server = http.createServer((req, res) => {
+  console.log(req);
+});
+
+// Node.js will not immediately exit when this function is called
+// Some optional arguments
+// port and hostname
+server.listen(3000);
