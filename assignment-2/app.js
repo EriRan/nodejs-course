bccdfac255c2eae82e0534222e4bb04a6cfc6da2
EriRan <erik.rantanen@hotmail.fi>
2023-01-17 12:56:36 +0200

    Fix element closing

--- assignment-2/app.js
+++ assignment-2/app.js
@@ -11,20 +11,20 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log("Second funnel");
-  res.send("<h1>Second funnel response</h1<");
+  res.send("<h1>Second funnel response</h1>");
 });
 */
 
 // Step 3
 app.get("/users", (req, res, next) => {
   console.log("/users middleware called")
-  res.write("<h1>/users middleware</h1<");
+  res.write("<h1>/users middleware</h1>");
   return res.end();
 });
 
 app.get("/", (req, res, next) => {
   console.log("/ middleware called")
-  res.write("<h1>/ middleware</h1<");
+  res.write("<h1>/ middleware</h1>");
   return res.end();
 });
 
