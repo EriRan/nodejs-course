f356d49ae057c23d0fe5d4709de0996e613126c7
EriRan <erik.rantanen@hotmail.fi>
2023-01-17 15:05:52 +0200

    Add root dir util

--- 1-node-server/routes/shop.js
+++ 1-node-server/routes/shop.js
@@ -3,11 +3,13 @@ const path = require("path");
 
 const router = express.Router();
 
+const rootDir = require("../util/path");
+
 router.get("/", (req, res, next) => {
   // No slashes to the path because path.join will automatically add them
   // Works in both Windows and Linux!!!!
   // This will be a pain to update if the package structure is changed
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
