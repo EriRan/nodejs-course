9ab5b7484b6b30ff71176ca1435efa53b76d1c84
EriRan <erik.rantanen@hotmail.fi>
2023-01-17 14:10:15 +0200

    Add /admin/ part to admin.js endpoints

--- 1-node-server/app.js
+++ 1-node-server/app.js
@@ -10,7 +10,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.use(adminRoutes);
+// admin prefix route
+// Inside the router using /admin in all URLs not required if calling express's router!!!
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 // 404 error page
--- 1-node-server/routes/admin.js
+++ 1-node-server/routes/admin.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+// admin/add-product
 router.get("/add-product", (req, res, next) => {
   res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add</button></form>"
+    "<form action='/admin/product' method='POST'><input type='text' name='title'><button type='submit'>Add</button></form>"
   );
 });
 
+// admin/product
 router.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
