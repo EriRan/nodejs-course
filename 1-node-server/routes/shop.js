a40d11175c2293282b753a46dd6a673bf4b8d1be
EriRan <erik.rantanen@hotmail.fi>
2023-01-17 14:51:05 +0200

    Use HTML templates

--- 1-node-server/routes/shop.js
+++ 1-node-server/routes/shop.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>Default page</h1>");
+  // No slashes to the path because path.join will automatically add them
+  // Works in both Windows and Linux!!!!
+  // This will be a pain to update if the package structure is changed
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
