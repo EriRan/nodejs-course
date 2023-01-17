58203a85e5f548fc86a1d44109f382c8825739fa
EriRan <erik.rantanen@hotmail.fi>
2023-01-17 12:48:28 +0200

    Add middlewares that get called only once

--- assignment-2/app.js
+++ assignment-2/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 // Step 2
+/*
 app.use((req, res, next) => {
   console.log("First funnel");
   next();
@@ -11,6 +12,17 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   console.log("Second funnel");
   res.send("<h1>Second funnel response</h1<");
+});
+*/
+
+app.get("/users", (req, res, next) => {
+  console.log("/users middleware called")
+  res.write("<h1>/users middleware</h1<");
+});
+
+app.get("/", (req, res, next) => {
+  console.log("/ middleware called")
+  res.write("<h1>/ middleware</h1<");
 });
 
 app.listen(3000);
