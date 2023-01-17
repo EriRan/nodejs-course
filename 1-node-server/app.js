be3da7a5532d5672ecfd6f98deaec8be9cd9e926
EriRan <erik.rantanen@hotmail.fi>
2023-01-17 12:07:53 +0200

    Add html response

--- 1-node-server/app.js
+++ 1-node-server/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log("In another middleware");
-  
+  res.send("<h1>Hello from Express!</h1<");
 });
 
 // Crucial method to creating a server
