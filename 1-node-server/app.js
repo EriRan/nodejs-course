1a2e7cfc93134cf973359352eef5c431398dc710
EriRan <erik.rantanen@hotmail.fi>
2023-01-17 14:00:01 +0200

    Add page not found middleware

--- 1-node-server/app.js
+++ 1-node-server/app.js
@@ -13,5 +13,10 @@ const shopRoutes = require("./routes/shop");
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// 404 error page
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+})
+
 // Shortcut for http.createServer
 app.listen(3000);
