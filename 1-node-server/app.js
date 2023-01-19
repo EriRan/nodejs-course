5f117d7e0c60bdfa1c6c1f27b41a408c6f65f40c
EriRan <erik.rantanen@hotmail.fi>
2023-01-19 09:09:10 +0200

    Use Pug layouts
    Reduces the amount of duplicate code

--- 1-node-server/app.js
+++ 1-node-server/app.js
@@ -27,7 +27,7 @@ app.use(shopRoutes);
 
 // 404 error page
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { pageTitle: "Product not found" });
 });
 
 // Shortcut for http.createServer
