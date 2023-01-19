bb39725bc5e6df3990aa8ced5dc194b27b1ac18e
EriRan <erik.rantanen@hotmail.fi>
2023-01-19 12:09:03 +0200

    Use express.urlencoded instead

--- assignment-4/app.js
+++ assignment-4/app.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 app.set("view engine", "pug");
 app.set("views", "views");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 const nameData = require("./routes/name");
 const listRoute = require("./routes/list");
