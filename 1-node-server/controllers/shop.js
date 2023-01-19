06ad12dbfd40368005bf4db92be02e55ca3a00b4
EriRan <erik.rantanen@hotmail.fi>
2023-01-19 15:35:14 +0200

    Display new variables in /

--- 1-node-server/controllers/shop.js
+++ 1-node-server/controllers/shop.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll((products) => {
+  return Product.fetchAll((products) => {
     res.render("shop/product-list", {
       prods: products,
       path: "/products",
@@ -15,7 +15,7 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const products = Product.fetchAll((products) => {
+  return Product.fetchAll((products) => {
     res.render("shop/index", {
       prods: products,
       path: "/",
