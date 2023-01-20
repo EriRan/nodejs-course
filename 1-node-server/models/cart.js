8a6843bd7c8db62b87752e2886a10811dd2c7281
EriRan <erik.rantanen@hotmail.fi>
2023-01-20 11:20:03 +0200

    Fix addProduct not working correctly

--- 1-node-server/models/cart.js
+++ 1-node-server/models/cart.js
@@ -24,7 +24,7 @@ module.exports = class Cart {
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
         cart.products = [...cart.products];
-        cart[existingProductIndex] = updatedProduct;
+        cart.products[existingProductIndex] = updatedProduct;
       } else {
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
