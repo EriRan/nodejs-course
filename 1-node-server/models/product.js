2387be930152aa157b614ec7b57757645caea8ac
EriRan <erik.rantanen@hotmail.fi>
2023-01-23 08:27:54 +0200

    Add to cart

--- 1-node-server/models/product.js
+++ 1-node-server/models/product.js
@@ -50,7 +50,6 @@ module.exports = class Product {
   static deleteById(id) {
     getProductsFromFile((products) => {
       const product = products.find(prod => prod.id === id);
-      console.log(id);
       const updatedProducts = products.filter((product) => product.id === id);
       fs.writeFile(p, JSON.stringify(updatedProducts), err => {
         if (!err) {
