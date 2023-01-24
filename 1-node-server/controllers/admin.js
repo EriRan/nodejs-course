154e4548e73f703d318e279335191677eab131e1
EriRan <erik.rantanen@hotmail.fi>
2023-01-24 14:01:41 +0200

    Use Sequelize method to create product
    This is some Javascript magic

--- 1-node-server/controllers/admin.js
+++ 1-node-server/controllers/admin.js
@@ -13,12 +13,15 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  })
+  // Sequelize adds automatically a method like this with the relationship
+  // MAGIC
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+    })
     .then((result) => {
       console.log("Product created");
       res.redirect("/admin/products");
