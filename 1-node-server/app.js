16722f9084de5e80f5a1c09e890ca22ac3e5565b
EriRan <erik.rantanen@hotmail.fi>
2023-01-24 14:01:14 +0200

    Include user with every request
    Cool

--- 1-node-server/app.js
+++ 1-node-server/app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 // app is a valid requestHandler so it can be passed to http.createServer
 const app = express();
@@ -19,6 +21,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Serve public folder statically so that this folder is available in eg. html files
 app.use(express.static(path.join(__dirname, "public")));
 
+// Register middleware
+// Only runs if npm server is started succesfully
+// User guaranteed to be found
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next(); // Need this so that user can be passed to next middlewares
+    })
+    .catch((err) => console.error(err));
+});
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errors = require("./controllers/error");
@@ -31,10 +45,22 @@ app.use(shopRoutes);
 // 404 error page
 app.use(errors.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync() // This overwrites the tables. Do not use this in production
   .then((result) => {
+    return User.findByPk(1);
     // Shortcut for http.createServer
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Max", email: "test@test.com" });
+    }
+    return Promise.resolve(user);
+  })
+  .then((user) => {
     app.listen(3000);
   })
   .catch((err) => {
