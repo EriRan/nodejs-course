3ab39a62ddbafeade5f3cae6dcec63943b7bf4f0
EriRan <erik.rantanen@hotmail.fi>
2023-01-26 13:59:24 +0200

    Display cart contents with Mongoose

--- 1-node-server/routes/shop.js
+++ 1-node-server/routes/shop.js
@@ -9,11 +9,11 @@ router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct);
 router.post("/cart", shopController.postCart);
+router.get("/cart", shopController.getCart);
 
 // Order of the router.gets is important. eg. /products/delete would clash with /products/:productId
 // TODO: Reimplement with Mongoose
 /*
-router.get("/cart", shopController.getCart);
 router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 router.post("/create-order", shopController.postOrder);
 router.get("/orders", shopController.getOrders);
