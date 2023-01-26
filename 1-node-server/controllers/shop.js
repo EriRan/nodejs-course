c7074a8a344415dbb692e3929984ed14f50f5e33
EriRan <erik.rantanen@hotmail.fi>
2023-01-26 14:45:03 +0200

    Create order with Mongoose

--- 1-node-server/controllers/shop.js
+++ 1-node-server/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -84,8 +85,24 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .addOrder()
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((item) => {
+        return { quantity: item.quantity, product: { ...item.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
     .then((result) => {
+      return req.user.clearCart();
+    })
+    .then(() => {
       return res.redirect("/orders");
     })
     .catch((err) => console.error(err));
--- 1-node-server/routes/shop.js
+++ 1-node-server/routes/shop.js
@@ -11,11 +11,12 @@ router.get("/products/:productId", shopController.getProduct);
 router.post("/cart", shopController.postCart);
 router.get("/cart", shopController.getCart);
 router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+router.post("/create-order", shopController.postOrder);
 
 // Order of the router.gets is important. eg. /products/delete would clash with /products/:productId
 // TODO: Reimplement with Mongoose
 /*
-router.post("/create-order", shopController.postOrder);
+
 router.get("/orders", shopController.getOrders);
 */
 
