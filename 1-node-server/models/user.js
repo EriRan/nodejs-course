95c227320e089a31fbc7a8290f0512ceaad50ea1
EriRan <erik.rantanen@hotmail.fi>
2023-01-26 08:32:54 +0200

    Display cart contents once more

--- 1-node-server/models/user.js
+++ 1-node-server/models/user.js
@@ -52,6 +52,27 @@ class User {
       );
   }
 
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((item) => item.productId);
+    // God damn
+    // Separate mapper classes or functions in the real world?
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((product) => {
+          return {
+            ...product,
+            quantity: this.cart.items.find((item) => {
+              return item.productId.toString() === product._id.toString();
+            }).quantity,
+          };
+        });
+      });
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
