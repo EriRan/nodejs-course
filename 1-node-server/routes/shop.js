52f1717a82a79759e72503adcdf38ccbc1d551de
EriRan <erik.rantanen@hotmail.fi>
2023-01-26 09:46:15 +0200

    Display orders

--- 1-node-server/routes/shop.js
+++ 1-node-server/routes/shop.js
@@ -14,10 +14,6 @@ router.post("/cart", shopController.postCart);
 router.get("/cart", shopController.getCart);
 router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 router.post("/create-order", shopController.postOrder);
-
-/*
-* TODO: Convert to MongoDb
 router.get("/orders", shopController.getOrders);
-*/
 
 module.exports = router;
