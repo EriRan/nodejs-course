887a05f5c94c50c3f2997a9edca8d0a5df5fbd01
EriRan <erik.rantanen@hotmail.fi>
2023-01-26 11:31:39 +0200

    Move user code into product

---
+++ 1-node-server/models/product.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+// Cool definitions
+// _id added automatically. No need to have it in the schema
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
