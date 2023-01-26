6cd4909c8696bafff94fc5da1b54e1569f48773e
EriRan <erik.rantanen@hotmail.fi>
2023-01-26 14:21:03 +0200

    Remove from cart with Mongoose

--- 1-node-server/routes/shop.js
+++ 1-node-server/routes/shop.js
@@ -10,11 +10,11 @@ router.get("/products", shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct);
 router.post("/cart", shopController.postCart);
 router.get("/cart", shopController.getCart);
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 
 // Order of the router.gets is important. eg. /products/delete would clash with /products/:productId
 // TODO: Reimplement with Mongoose
 /*
-router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 router.post("/create-order", shopController.postOrder);
 router.get("/orders", shopController.getOrders);
 */
