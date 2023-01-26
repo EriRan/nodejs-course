0721d389fb70de87daa19ad8db7b55d2b2d461eb
EriRan <erik.rantanen@hotmail.fi>
2023-01-26 14:56:14 +0200

    Display orders with Mongoose

--- 1-node-server/routes/shop.js
+++ 1-node-server/routes/shop.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 const shopController = require("../controllers/shop");
 
+// Order of the router.gets is important. eg. /products/delete would clash with /products/:productId
 router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct);
@@ -12,12 +13,6 @@ router.post("/cart", shopController.postCart);
 router.get("/cart", shopController.getCart);
 router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 router.post("/create-order", shopController.postOrder);
-
-// Order of the router.gets is important. eg. /products/delete would clash with /products/:productId
-// TODO: Reimplement with Mongoose
-/*
-
 router.get("/orders", shopController.getOrders);
-*/
 
 module.exports = router;
