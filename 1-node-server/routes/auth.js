094722c4084a9dcc008b5ea78cc7b25df0066cc0
EriRan <erik.rantanen@hotmail.fi>
2023-01-30 15:11:42 +0200

    Add password format validation

--- 1-node-server/routes/auth.js
+++ 1-node-server/routes/auth.js
@@ -3,7 +3,7 @@ const path = require("path");
 const authController = require("../controllers/auth");
 
 const express = require("express");
-const { check } = require("express-validator/check");
+const { check, body } = require("express-validator/check");
 
 const router = express.Router();
 
@@ -13,16 +13,22 @@ router.post("/login", authController.postLogin);
 router.post("/logout", authController.postLogout);
 router.post(
   "/signup",
-  check("email")
-    .isEmail()
-    .withMessage("Please enter a valid email")
-    .custom((value, { req }) => {
-      // Example of a custom validator
-      if (value === "test@test.com") {
-        throw new Error("This email is not allowed");
-      }
-      return true;
-    }),
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        // Example of a custom validator
+        if (value === "test@test.com") {
+          throw new Error("This email is not allowed");
+        }
+        return true;
+      }),
+    body("password", "Password has to be 5 or more characters and alphanumeric")
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
+
   authController.postSignup
 );
 router.get("/reset", authController.getReset);
