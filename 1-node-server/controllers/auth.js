9e0d31bd23449cd9eb1becfd5e90a2a963d1cf3a
EriRan <erik.rantanen@hotmail.fi>
2023-01-30 15:57:58 +0200

    Remove TODO

--- 1-node-server/controllers/auth.js
+++ 1-node-server/controllers/auth.js
@@ -85,8 +85,6 @@ exports.postLogout = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
-  // TODO: input validation in a separate module
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).render("auth/signup", {
