7ef01ffea7ef7f4a05bcd2e9b43e0047cd24fb5e
EriRan <erik.rantanen@hotmail.fi>
2023-02-01 09:57:38 +0200

    Add comment

--- 1-node-server/util/file.js
+++ 1-node-server/util/file.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+// "fire and forget". Caller will not wait for this function to finish
 const deleteFile = (filePath) => {
   fs.unlink(filePath, (err) => {
     if (err) {
