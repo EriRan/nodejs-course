f564067957494e1223e62b33c82a647d0ecbdc22
EriRan <erik.rantanen@hotmail.fi>
2023-02-01 08:39:07 +0200

    Open pregenerated PDF when invoice is clicked

--- 1-node-server/controllers/shop.js
+++ 1-node-server/controllers/shop.js
@@ -1,5 +1,7 @@
 const Product = require("../models/product");
 const Order = require("../models/order");
+const fs = require("fs");
+const path = require("path");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -152,4 +154,20 @@ exports.getOrders = (req, res, next) => {
         return next(error);
       });
   }
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = "invoice-" + orderId + ".pdf";
+  const invoicePath = path.join("data", "invoices", invoiceName);
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    // Set file for download
+    // attachment instead of inline would download it
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", "inline; filename=" + invoiceName);
+    res.send(data);
+  });
 };
