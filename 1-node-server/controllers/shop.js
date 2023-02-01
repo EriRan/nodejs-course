ac537ba9e6dda630fe062f03a083d4b6d2d5a221
EriRan <erik.rantanen@hotmail.fi>
2023-02-01 09:14:23 +0200

    Use read stream

--- 1-node-server/controllers/shop.js
+++ 1-node-server/controllers/shop.js
@@ -168,16 +168,13 @@ exports.getInvoice = (req, res, next) => {
       }
       const invoiceName = "invoice-" + orderId + ".pdf";
       const invoicePath = path.join("data", "invoices", invoiceName);
-      fs.readFile(invoicePath, (err, data) => {
-        if (err) {
-          return next(err);
-        }
-        // Set file for download
-        // attachment instead of inline would download it
-        res.setHeader("Content-Type", "application/pdf");
-        res.setHeader("Content-Disposition", "inline; filename=" + invoiceName);
-        res.send(data);
-      });
+      // Stream file
+      // Data will be downloaded by the browser step by step
+      // This is better for server memory consumption
+      const file = fs.createReadStream(invoicePath);
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader("Content-Disposition", "inline; filename=" + invoiceName);
+      file.pipe(res);
     })
     .catch((err) => next(err));
 };
