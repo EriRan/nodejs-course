9289c54f3123debec8befa810e351c9e9afe0fbd
EriRan <erik.rantanen@hotmail.fi>
2023-02-01 13:24:37 +0200

    Improve comment

--- 1-node-server/public/js/admin.js
+++ 1-node-server/public/js/admin.js
@@ -4,7 +4,8 @@ const deleteProduct = (btn) => {
 
   const productElement = btn.closest("article");
 
-  // This request will end up in the admin router
+  // Here we send a request to backend and when we get a response, we manually remove the HTML element that held the product that this function was
+  // called with
   fetch("/admin/product/" + prodId, {
     method: "DELETE",
     headers: {
