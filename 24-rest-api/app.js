f206b5bd2f274d18ca4fb8e4143243209bf31fb7
EriRan <erik.rantanen@hotmail.fi>
2023-02-02 12:55:22 +0200

    Add headers to stop CORS error

--- 24-rest-api/app.js
+++ 24-rest-api/app.js
@@ -7,6 +7,14 @@ const app = express();
 // Configuration must be added before routes!!!
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+  // Instructor used wildcard instead of codepen.io here. That is very SUS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-HEADERS", "Content-Type, Authorization");
+  next();
+})
+
 app.use("/feed", feedRoutes);
 
 // Different port this time
