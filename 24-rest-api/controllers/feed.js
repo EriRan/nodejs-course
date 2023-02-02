0bed89123b2e6b93e94b2f5082cde896a783fc99
EriRan <erik.rantanen@hotmail.fi>
2023-02-02 13:56:50 +0200

    Display placeholder post

--- 24-rest-api/controllers/feed.js
+++ 24-rest-api/controllers/feed.js
@@ -2,7 +2,18 @@ export function getPosts(req, res, next) {
   // No more rendering at REST API
   // Status is more important now when the client does the rendering and it needs to react to the data received
   res.status(200).json({
-    posts: [{ title: "First post", content: "This is a first post" }],
+    posts: [
+      {
+        _id: 234234,
+        title: "First post",
+        content: "This is a first post",
+        imageUrl: "images/7.jpg",
+        creator: {
+          name: "Mold-Max",
+        },
+        date: new Date(),
+      },
+    ],
   });
 }
 
