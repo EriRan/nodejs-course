e435139d10523a1c960c72060fdef790a9ef2e62
EriRan <erik.rantanen@hotmail.fi>
2023-03-11 16:14:42 +0200

    Add testing with stubs

--- 30-testing/test/auth-middleware.js
+++ 30-testing/test/auth-middleware.js
@@ -1,5 +1,8 @@
 const authMiddleware = require("../middleware/is-auth");
 const expect = require("chai").expect;
+const jwt = require("jsonwebtoken");
+const sinon = require("sinon");
+
 // Group tests. Describe adds a header to test run results
 describe("Auth middleware", function () {
   it("should throw an error if no authorization header is present", function () {
@@ -18,6 +21,30 @@ describe("Auth middleware", function () {
     const req = {
       get: function () {
         return "xyz";
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should yield a userId after decoding the token", function () {
+    const req = {
+      get: function () {
+        return "Bearer sdfsdfsdfsdfsdfsdf";
+      },
+    };
+    sinon.stub(jwt, "verify");
+    jwt.verify.returns({ userId: "abc" });
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property("userId");
+    expect(req).to.have.property("userId", "abc");
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
+  });
+
+  it("should throw an erorr if the token cannot be verified", function () {
+    const req = {
+      get: function () {
+        return "Bearer xyz";
       },
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
