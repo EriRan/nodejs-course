11d1f56580ce7df6827aa316ca08817ada0cde9d
EriRan <erik.rantanen@hotmail.fi>
2023-03-11 15:36:16 +0200

    Replace dummy test with a real one

---
+++ 30-testing/test/auth-middleware.js
@@ -0,0 +1,12 @@
+const authMiddleware = require("../middleware/is-auth");
+const expect = require("chai").expect;
+
+it("should throw an error if no authorization header is present", function() {
+  const req = {
+    get: function() {
+      return null;
+    }
+  };
+  // This is weird
+  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw("Not authenticated.");
+})
