905469267c180bb6d5c839ba3ee56de1bbf82a2f
EriRan <erik.rantanen@hotmail.fi>
2023-03-11 17:42:22 +0200

    Add user save testing

--- 30-testing/test/auth-controller.js
+++ 30-testing/test/auth-controller.js
@@ -1,11 +1,36 @@
 const expect = require("chai").expect;
 const sinon = require("sinon");
+const mongoose = require("mongoose");
 
 const User = require("../models/user");
 const AuthController = require("../controllers/auth");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // Group tests. Describe adds a header to test run results
-describe("Auth Controller - Login", function () {
+describe("Auth Controller", function () {
+  // Ran once before starting the tests
+  before(function (done) {
+    mongoose
+      .connect(
+        `mongodb+srv://${process.env.mongodb_user}:${process.env.mongodb_password}@${process.env.mongodb_cluster_address}/test-messages?retryWrites=true`
+      )
+      .then((result) => {
+        const user = new User({
+          email: "test@test.com",
+          password: "tester",
+          name: "Test",
+          posts: [],
+          _id: "5c0f66b979af55031b34728a",
+        });
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
   it("should throw an error with code 500 if accessing the database fails", function (done) {
     sinon.stub(User, "findOne");
     User.findOne.throws();
@@ -13,13 +38,13 @@ describe("Auth Controller - Login", function () {
     const req = {
       body: {
         email: "test@test.com",
-        password: "tester"
-      }
-    }
+        password: "tester",
+      },
+    };
 
     // Mocka doesn't wait for async code to resolve
     // This is the same of what I encountered in the last project
-    AuthController.login(req, {}, () => {}).then(result => {
+    AuthController.login(req, {}, () => {}).then((result) => {
       expect(result).to.be.an("error");
       expect(result).to.have.property("statusCode", 500);
       // Signal to finish the test
@@ -28,5 +53,36 @@ describe("Auth Controller - Login", function () {
 
     User.findOne.restore();
     // Test does not end here: it ends inside the above async block
+  });
+
+  it("should send a response with a valid user status for an existing user", function (done) {
+    const req = { userId: "5c0f66b979af55031b34728a" };
+    const res = {
+      status: 500,
+      userStatus: null,
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (data) {
+        this.userStatus = data.status;
+      },
+    };
+    AuthController.getUserStatus(req, res, () => {}).then(() => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.userStatus).to.be.equal("I am new!"); // Should have default status
+      done();
+    });
+  });
+
+  // Ran once after all the tests
+  after(function (done) {
+    User.deleteMany({}) // Delete everything at the test database
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .then(() => {
+        done();
+      });
   });
 });
