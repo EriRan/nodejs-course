4d7be81a06ffda012d2ca56c0934d899528aeb30
EriRan <erik.rantanen@hotmail.fi>
2023-03-11 15:48:25 +0200

    Add test grouping

--- 30-testing/test/auth-middleware.js
+++ 30-testing/test/auth-middleware.js
@@ -1,12 +1,25 @@
 const authMiddleware = require("../middleware/is-auth");
 const expect = require("chai").expect;
+// Group tests. Describe adds a header to test run results
+describe("Auth middleware", function () {
+  it("should throw an error if no authorization header is present", function () {
+    const req = {
+      get: function () {
+        return null;
+      },
+    };
+    // This is weird
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated."
+    );
+  });
 
-it("should throw an error if no authorization header is present", function() {
-  const req = {
-    get: function() {
-      return null;
-    }
-  };
-  // This is weird
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw("Not authenticated.");
-})
+  it("should throw an error if the authorization header is only one string", function () {
+    const req = {
+      get: function () {
+        return "xyz";
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+});
