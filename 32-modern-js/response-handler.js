4e29eb4a46bc63501137b8244ca574c96c9fe6f7
EriRan <erik.rantanen@hotmail.fi>
2023-04-08 12:21:44 +0300

    Use res.sendFile instead
    Need to use a special trick to get previously global
    Node variable to be available again

--- 32-modern-js/response-handler.js
+++ 32-modern-js/response-handler.js
@@ -1,9 +1,10 @@
-import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url)); // Pseudo global variable
 
 const resHandler = (req, res, next) => {
-  fs.readFile("my-page.html", "utf8", (err, data) => {
-    res.send(data);
-  });
+  res.sendFile(path.join(__dirname, "my-page.html"));
 };
 
 export default resHandler;
