bbccabcf14a63bc2ff9d09f9e17091d5c7283d68
EriRan <erik.rantanen@hotmail.fi>
2023-04-16 16:22:39 +0300

    Write to a file
    Outputs a permission request. Feature of Deno

--- 34-deno/app.ts
+++ 34-deno/app.ts
@@ -1,5 +1,7 @@
-let message: string;
-
-message = "Hello!";
-
-console.log(message);
+const text = "This is a file write test";
+const encoder = new TextEncoder();
+const data = encoder.encode(text);
+// Returns a promise
+Deno.writeFile("message.txt", data).then(() => {
+  console.log("Wrote to a file!");
+});
